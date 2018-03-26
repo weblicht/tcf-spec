461ca2c1f1c9799461efd424a59d0bb49df22672
Mohammad Fazleh Elahi <fazleh2010@gmail.com>
2018-03-26 15:02:11 +0200

    testing previous and extended schema

--- src/test/java/eu/clarin/webleaf/validate/ValidateExamplesTest.java
+++ src/test/java/eu/clarin/webleaf/validate/ValidateExamplesTest.java
@@ -29,7 +29,8 @@ public class ValidateExamplesTest {
     private static final String[] INPUT_FILES_FOR_READ = new String[]{
         "/corpus.xml",
         "/corpus_extdata.xml",
-        "/tcf04-karin-wl_1_1.xml",
+        "/tcf04-karin-wl.xml",
+        "/tcf04-karin-wl_extension.xml",
         "/lex04-karin-wl.xml"
     };
     public static final String WebLEAF_SCHEMA_LOCATION = "/d-spin-local_0_4.rnc";
